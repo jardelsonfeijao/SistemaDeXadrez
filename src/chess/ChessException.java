bc03166d882b000ffdd4f4024f32147c74ec2111
Jardelson Feijao <jardelsonfeijao04@gmail.com>
2022-03-06 12:30:48 -0300

    ChessException and chessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+ static final long serialVersionUID = 1L;
+
+ public ChessException(String msg) {
+	 super(msg);
+ }
+ 
+ 
+}
