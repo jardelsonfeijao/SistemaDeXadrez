95ccc5485254cb25f51c9ac6caf4db36f110d773
Jardelson Feijao <jardelsonfeijao04@gmail.com>
2022-03-09 22:46:56 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -46,9 +47,17 @@ public class UI {
 			throw new InputMismatchException("Error reading ChessPosition. valid values are from a1 to be h8.");
 		}
 	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
-		for (int i = 0; i < pieces.length; i++) {
+		System.out.println("  a b c d e f g h");
+		for (int i = 0; i < pieces.length; i++) {			
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], false);
@@ -59,6 +68,7 @@ public class UI {
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		System.out.println("  a b c d e f g h");
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
